6ee7eaef3d3adcc995b00968be0b8f1c70a3ee05
Angela292005 <71849594@continental.edu.pe>
2025-04-23 11:48:23 -0500

    Creando el esquelto de las clases Conjunto y TestConjunto

--- src/logica/Conjunto.py
+++ src/logica/Conjunto.py
@@ -1,12 +1,5 @@
-def __init__(self):
-    self.numeros = []
-
-
-def agregar_numero(self, numero):
-    self.numeros.append(numero)
-
-
-def calcular_promedio(self):
-    if not self.numeros:
-        return 0
-    return sum(self.numeros) / len(self.numeros)
+class Conjunto:
+    def __init__(self, conjunto):
+        self.__conjunto=conjunto
+    def promedio( self ):
+        return False
